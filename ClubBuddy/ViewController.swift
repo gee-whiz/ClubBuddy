51b934de9218e90b3e34f36953955aad5b70ee0d
George <george@immedia.co.za>
2017-07-10 20:25:15 +0200

    initial commit

---
+++ ClubBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClubBuddy
+//
+//  Created by George Kapoya on 2017/07/10.
+//  Copyright © 2017 georgekapoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
